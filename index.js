8ac5f7f659dbb531845b23c4359d4e63b32344cc
sid-al <sendplusx@gmail.com>
2022-06-11 10:34:26 +0300

    commit tree 894841ff6f804859d3b5e7d7af30245b63132a07

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+ //   tutorials.map( line => {
+  //   const Letterintials = line.split(' ');
+  //   const capitalizeLetterIntials = Letterintials.map( 
+  //     Letterintials => Letterintials.charAt(0).toUpperCase() + Letterintials.slice(1))
+  // });
+
+  // const feedback = capitalizeLetterIntials.join(' ');
+  // return feedback;
+
+
+// const titleCased = () => {
+//   return tutorials.map( CasedArray => {
+//     CasedArray.split(' ').map(CasedArray => CasedArray.charAt(0).toUpperCase() + CasedArray.slice(1)).join('')
+//   })
+// };
+
 
 const titleCased = () => {
-  return tutorials
+  // tutorials.map((newArray)=>{
+  //   newArray.split(' ').
+  // })
+  return tutorials.map(newArray => (newArray.split(' ').map(newArray => newArray.charAt(0).toUpperCase() + newArray.slice(1))).join(" "))
 }
+
